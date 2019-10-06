902b615409ee2296f820c88f9406991671758456
showall <gerardsho@gmail.com>
2019-10-06 21:12:35 +0800

    Update 3.2.2.4.R

--- 3.2.2.4.R
+++ 3.2.2.4.R
@@ -1,7 +1,7 @@
 
 # The BEER - BEHAVIOURAL EQUILIBRIUM EXCHANGE RATE model was developed by Clark and MacDonald (1999) and estimates the fair value of currencies according to short, medium and long-run determinants. 
 # An important concept is that there is no established theory for the choice of economic variables; hence, the choice of variables is based on economic intuition and data simplicity and availability. 
-# In this exercise we chose the inflation differential, interest rate diferential and current payment differential to be regression variables for the ln(return) fo the AUD?USD exchange rate.  
+# In this exercise we chose the inflation differential, interest rate diferential and to be regression variables for the ln(return) for the AUD?USD exchange rate.  
 
 install.packages("urca")
 install.packages("vars")
@@ -16,20 +16,20 @@ library(tsDyn)
 
 DF <- as.data.frame(read_excel(path = "Data-VEC Model.xlsx",sheet="Data"))
 
-data = as.data.frame(cbind(DF$`ln( return )`, DF$`Infl Diff`, DF$`IR Diff`, DF$`CP Diff`))
+data = as.data.frame(cbind(DF$`ln( return )`, DF$`Infl Diff`, DF$`IR Diff`))
 
 
 #COMMENT : Test for stationarity
 colnames(data)[1]<-"Rate"
 colnames(data)[2]<-"Inflation"
 colnames(data)[3]<-"Interest"
-colnames(data)[4]<-"Current_Payment"
+
 
 
 dif_Rate=diff(data$Rate)
 dif_Inflation=diff(data$Inflation)
 dif_Interest=diff(data$Interest)
-dif_Current_Payment=diff(data$Current_Payment)
+
 
 # COMMENT : Using ADF test to check for stationarity
 adf.test(data$Rate)
@@ -41,8 +41,7 @@ adf.test(dif_Inflation)
 adf.test(data$Interest)
 adf.test(dif_Interest)
 
-adf.test(data$Current_Payment)
-adf.test(dif_Current_Payment)
+
 
 #COMMENT : From the result, we can confirm the stationarity of order 1 for the variables.
 
@@ -50,8 +49,8 @@ adf.test(dif_Current_Payment)
 
 VARselect(data,lag.max=10, type="none")$selection
 
-#COMMENT : Based on the AIC result, we can pick the lag = 4
-#We can then proceed we the Johansen test using k=4
+#COMMENT : Based on the AIC result, we can pick the lag = 9
+#We can then proceed we the Johansen test using k=9
 
 jotest1a=ca.jo(data, type="eigen", K=4, ecdet="none", spec="longrun") 
 summary(jotest1a) 
@@ -65,11 +64,13 @@ summary(jotest2a)
 
 #VECM model implementation
 
-model = VECM(data,4,r = 1, include = "const",estim = "ML", LRinclude = "none") 
+model = VECM(data,9,r = 1, include = "const",estim = "ML", LRinclude = "none") 
 summary(model)
 
-#COMMENT on the result : From the results, we can deduce that there is a long term equilibrium between AUD/USD exchange rate and interest rate differential and also current payment differential from the significant ECT.
-#Short term wise, the exchange rate is only seem affected by lag-2 quarters current payment account.
+#COMMENT on the result : From the results, we can deduce that there is a long term equilibrium between AUD/USD exchange rate and interest rate differential and also inflation differential from the significant ECT.
+#Both the ECM are significant, even though the ECM, which represents the speed of adjustment for interest rate is showing positive, the overall multivariate system will still be drawn back to equilibrium as the reduction effect on inflation is greater.
+#Short term wise, the exchange rate does not seem to be heavily affected by past data. 
+#The cointegrating equation shows the interest rate helps in appreciation of AUD/USD while inflation rate is more related to its depreciation.
 
 
 
