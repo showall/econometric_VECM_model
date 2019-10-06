4fe3e28cb9d66cdd6807f554d8ff10fde0d87240
showall <gerardsho@gmail.com>
2019-10-06 20:00:34 +0800

    Update 3.2.2.4.R

--- 3.2.2.4.R
+++ 3.2.2.4.R
@@ -15,7 +15,7 @@ DF <- as.data.frame(read_excel(path = "Data-VEC Model.xlsx",sheet="Data"))
 data = as.data.frame(cbind(DF$`ln( return )`, DF$`Infl Diff`, DF$`IR Diff`, DF$`CP Diff`))
 
 
-#1. test for stationarity
+#COMMENT : Test for stationarity
 colnames(data)[1]<-"Rate"
 colnames(data)[2]<-"Inflation"
 colnames(data)[3]<-"Interest"
@@ -27,7 +27,7 @@ dif_Inflation=diff(data$Inflation)
 dif_Interest=diff(data$Interest)
 dif_Current_Payment=diff(data$Current_Payment)
 
-# Using ADF test to check for stationarity
+# COMMENT : Using ADF test to check for stationarity
 adf.test(data$Rate)
 adf.test(dif_Rate)
 
@@ -39,14 +39,15 @@ adf.test(dif_Interest)
 
 adf.test(data$Current_Payment)
 adf.test(dif_Current_Payment)
-#confirmed stationarity of order 1
 
-#2. estimate the lag for VECM Model 
+#COMMENT : From the result, we can confirm the stationarity of order 1 for the variables.
+
+#COMMENT : Etimate the lag for VECM Model 
 
 VARselect(data,lag.max=10, type="none")$selection
 
-#pick lag = 4 based on AIC
-# Johansen test 
+#COMMENT : Based on the AIC result, we can pick the lag = 4
+#We can then proceed we the Johansen test using k=4
 
 jotest1a=ca.jo(data, type="eigen", K=4, ecdet="none", spec="longrun") 
 summary(jotest1a) 
@@ -54,15 +55,16 @@ summary(jotest1a)
 jotest2a=ca.jo(data, type="trace", K=4, ecdet="none", spec="longrun") 
 summary(jotest2a)
 
-#there are cointegration relations as both tests rejects the null hypothesis of r = 0 at 5%
-#r at most = 1
+#COMMENT : Both tests are showing there are cointegration relations as both tests rejects the null hypothesis of r = 0 at 5%
+#r or number of co-integrations is at most = 1
 
 #VECM model implementation
-
 
 model = VECM(data,4,r = 1, include = "const",estim = "ML", LRinclude = "none") 
 summary(model)
 
-#optional for prediction
-#newdata = tail(data,5)
-#predict(VECM_fit,newdata = newdata)
+#COMMENT on the result : From the results, we can deduce that there is a long term equilibrium between AUD/USD exchange rate and interest rate differential and also current payment differential from the significant ECT.
+#Short term wise, the exchange rate is only seem affected by lag-2 quarters current payment account.
+
+
+
